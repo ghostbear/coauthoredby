977308721f40850865e27d75518e938fce63a819
Andreas <andreas.everos@gmail.com>
2021-04-05 22:10:19 +0200

    Support multiple co-authors 🎷

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,54 @@
 <script>
-	export let username;
-	$: snippet = `Co-authored-by: ${username} <${username}@users.noreply.github.com>`
+	let coAuthors = [
+		{ username: "ghost" }
+	]
+
+	function add() {
+		coAuthors = coAuthors.concat([{ username: "" }])
+	}
+
+	function reset() {
+		coAuthors = [{ username: "" }]
+	}
+
+	function remove(coAuthor) {
+		coAuthors = coAuthors.filter(author => author !== coAuthor)
+	}
 
 	function copyToClipboard() {
 		navigator.clipboard.writeText(snippet)
 	}
+
+	$: commitMessage = coAuthors.filter(author => author.username.length > 0).map(author => `Co-authored-by: ${author.username} &lt;${author.username}@users.noreply.github.com&gt;`).join("<br>")
 </script>
 
 <main>
 	<h1>Co-authored</h1>
 	<p>Simple tool to give credit to users who help you with a commit on GitHub</p>
 	<br>
-	<b>What is the username of the co-author?</b>
-	<div>
-		<input bind:value={username} />	
+	<b>What is the username of the co-author(s)?</b>
+	<div class="co-authors">
+		{#each coAuthors as coAuthor}
+			<div class="co-author">
+				<input placeholder="username" bind:value={coAuthor.username}>
+				<button on:click={remove(coAuthor)}>
+					Remove
+				</button>
+			</div>
+		{/each}
 	</div>
-	
-	{#if username}
-		<code id="co-author" on:click={copyToClipboard}>
-			{snippet}
-		</code>
+
+	<div>
+		<button on:click={add}>
+			Add new
+		</button>
+		<button on:click={reset}>
+			Reset
+		</button>
+	</div>
+
+	{#if commitMessage}
+		<code id="co-author" on:click={copyToClipboard} contenteditable="true" bind:innerHTML={commitMessage}></code>
 	{/if}
 </main>
 

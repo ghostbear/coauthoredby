9940f773ad2fdfb614eff48f3bae77df29f22696
Andreas E <andreas.everos@gmail.com>
2021-04-05 23:17:21 +0200

    Add prettier and add line break to snippet 🎺

--- src/App.svelte
+++ src/App.svelte
@@ -1,107 +1,106 @@
 <script>
-	import { Octokit } from "@octokit/core";
+  import { Octokit } from "@octokit/core";
 
-	let coAuthors = [
-		{ username: "ghost" }
-	]
+  let coAuthors = [{ username: "ghost" }];
 
-	let commitMessage = ""
+  let commitMessage = [];
 
-	function add() {
-		coAuthors = coAuthors.concat([{ username: "" }])
-	}
+  $: snippet = commitMessage.join("<br>");
 
-	function reset() {
-		coAuthors = [{ username: "" }]
-	}
+  function add() {
+    coAuthors = coAuthors.concat([{ username: "" }]);
+  }
 
-	function remove(coAuthor) {
-		coAuthors = coAuthors.filter(author => author !== coAuthor)
-	}
+  function reset() {
+    coAuthors = [{ username: "" }];
+  }
 
-	function submit() {
-		var t0 = performance.now()
-		Promise.all(client.getAuthors(coAuthors))
-			.then(values => {
-				commitMessage = values.map(value => {
-					let username = value.data.login
-					let id = value.data.id
-					return `Co-authored-by: ${username} <${id}+${username}@users.noreply.github.com>`
-				}).join("\n")
-			}) 
-			var t1 = performance.now()
-			console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")e
-	}
+  function remove(coAuthor) {
+    coAuthors = coAuthors.filter((author) => author !== coAuthor);
+  }
 
-	function copyToClipboard() {
-		navigator.clipboard.writeText(snippet)
-	}
+  function submit() {
+    Promise.all(client.getAuthors(coAuthors)).then((values) => {
+      commitMessage = values.map((value) => {
+        let username = value.data.login;
+        let id = value.data.id;
+        return `Co-authored-by: ${username} <${id}+${username}@users.noreply.github.com>`;
+      });
+    });
+  }
 
-	const client = (() => {
-		const octokit = new Octokit();
-		const authors = new Map()
-		return {
-			getAuthors: (list) => {
-				return list.map(author => {
-					console.log(authors)
-					return (authors[author.username] = authors[author.username] ?? octokit.request('GET /users/{username}', {username: author.username}))
-				})
-			}
-		}
-	})()
+  function copyToClipboard() {
+    navigator.clipboard.writeText(snippet);
+  }
+
+  const client = (() => {
+    const octokit = new Octokit();
+    const authors = new Map();
+    return {
+      getAuthors: (list) => {
+        return list.map((author) => {
+          console.log(authors);
+          return (authors[author.username] =
+            authors[author.username] ??
+            octokit.request("GET /users/{username}", {
+              username: author.username,
+            }));
+        });
+      },
+    };
+  })();
 </script>
 
 <main>
-	<h1>Co-authored</h1>
-	<p>Simple tool to give credit to users who help you with a commit on GitHub</p>
-	<br>
-	<b>What is the username of the co-author(s)?</b>
-	<div class="co-authors">
-		{#each coAuthors as coAuthor}
-			<div class="co-author">
-				<input placeholder="username" bind:value={coAuthor.username}>
-				<button on:click={remove(coAuthor)}>
-					Remove
-				</button>
-			</div>
-		{/each}
-	</div>
+  <h1>Co-authored</h1>
+  <p>
+    Simple tool to give credit to users who help you with a commit on GitHub
+  </p>
+  <br />
+  <b>What is the username of the co-author(s)?</b>
+  <div class="co-authors">
+    {#each coAuthors as coAuthor}
+      <div class="co-author">
+        <input placeholder="username" bind:value={coAuthor.username} />
+        <button on:click={remove(coAuthor)}> Remove </button>
+      </div>
+    {/each}
+  </div>
 
-	<div>
-		<button on:click={add}>
-			Add new
-		</button>
-		<button on:click={reset}>
-			Reset
-		</button>
-		<button on:click={submit}>
-			Submit
-		</button>
-	</div>
+  <div>
+    <button on:click={add}> Add new </button>
+    <button on:click={reset}> Reset </button>
+    <button on:click={submit}> Submit </button>
+  </div>
 
-	{#if commitMessage}
-		<code id="co-author" on:click={copyToClipboard} contenteditable="true" bind:innerHTML={commitMessage}></code>
-	{/if}
+  {#if commitMessage}
+    <code
+      id="co-author"
+      on:click={copyToClipboard}
+      contenteditable="true"
+      bind:innerHTML={snippet}
+    />
+  {/if}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
